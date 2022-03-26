179251ce5a63211ad6ff6f9015f559e7000b019d
Stavros-Kyriacou <60002156+Stavros-Kyriacou@users.noreply.github.com>
2022-03-26 13:29:33 +1100

    create webapi

---
+++ BasketballApi/Controllers/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BasketballApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayerController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<PlayerController> _logger;
+
+        public PlayerController(ILogger<PlayerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Player> Get()
+        {
+            return null;
+        }
+    }
+}
---
+++ BasketballApi/Player.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace BasketballApi
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Team { get; set; }
+    }
+}
