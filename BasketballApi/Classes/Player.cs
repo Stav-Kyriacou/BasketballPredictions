b5b9835e7e28cc3342d230e9c1995a1f5fb8bf7f
Stavros-Kyriacou <60002156+Stavros-Kyriacou@users.noreply.github.com>
2022-03-27 12:42:52 +1100

    player id

--- BasketballApi/Classes/Player.cs
+++ BasketballApi/Classes/Player.cs
@@ -4,6 +4,7 @@ namespace BasketballApi
 {
     public class Player
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Team { get; set; }
     }
