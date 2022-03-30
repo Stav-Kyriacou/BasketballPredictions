3b176ff36131ed0d646c339e5c30564cd5c6ab5c
Stavros-Kyriacou <60002156+Stavros-Kyriacou@users.noreply.github.com>
2022-03-30 22:44:13 +1100

    import project

--- BasketballApp/src/app/app.module.ts
+++ BasketballApp/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayerComponent } from './components/player/player.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ BasketballApp/src/app/models/player.ts
@@ -0,0 +1,12 @@
+export interface Player {
+    playerName: string;
+    team: string;
+    points: number;
+    rebounds: number;
+    blocks: number;
+    steals: number;
+    assists: number;
+    fieldGoalsMade: number;
+    freeThrowsMade: number;
+    efficiency: number;
+  }
