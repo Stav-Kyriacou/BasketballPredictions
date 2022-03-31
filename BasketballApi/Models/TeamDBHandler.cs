257eaaf9e4868210d900f6aaaa7ea1656caf2228
Stavros-Kyriacou <60002156+Stavros-Kyriacou@users.noreply.github.com>
2022-03-31 14:41:32 +1100

    create team db handler

---
+++ BasketballApi/Models/TeamDBHandler.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace BasketballApi
+{
+    public class TeamDBHandler : DatabaseHandler
+    {
+        
+    }
+}
