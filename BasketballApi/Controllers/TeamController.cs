97e6956d8cbe8fd0e861764db0603856bcea1d68
Stavros-Kyriacou <60002156+Stavros-Kyriacou@users.noreply.github.com>
2022-03-31 14:41:23 +1100

    create team controller

---
+++ BasketballApi/Controllers/TeamController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Cors;
+
+namespace BasketballApi
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TeamController : ControllerBase
+    {
+        private readonly ILogger<TeamController> _logger;
+        private TeamDBHandler teamDBHandler = new TeamDBHandler();
+
+        public TeamController(ILogger<TeamController> logger)
+        {
+            _logger = logger;
+        }
+
+        [EnableCors("MyPolicy")]
+        [HttpGet]
+        [Route("/teams")]
+        public IEnumerable<Team> GetAllTeams()
+        {
+            return null;
+        }
+        [EnableCors("MyPolicy")]
+        [HttpPost]
+        public void PostTeam()
+        {
+            
+        }
+    }
+}
