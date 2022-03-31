2c238e950d75f6e22b7de4f2efa1d77bddfd26fb
Stavros-Kyriacou <60002156+Stavros-Kyriacou@users.noreply.github.com>
2022-03-31 14:41:07 +1100

    create team model

---
+++ BasketballApi/Models/Team.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BasketballApi
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateMade { get; set; }
+    }
+}
