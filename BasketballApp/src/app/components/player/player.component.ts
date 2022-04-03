e9f261cf92394fb485282b6236e377a88072de50
Stavros-Kyriacou <60002156+Stavros-Kyriacou@users.noreply.github.com>
2022-04-03 15:19:35 +1000

    player table pagination and filter

--- BasketballApp/src/app/components/player/player.component.ts
+++ BasketballApp/src/app/components/player/player.component.ts
@@ -1,5 +1,7 @@
 
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { Player } from 'src/app/models/player';
 import { PlayerService } from '../../services/player.service';
 
@@ -9,12 +11,32 @@ import { PlayerService } from '../../services/player.service';
   styleUrls: ['./player.component.css']
 })
 export class PlayerComponent implements OnInit {
+  columnsToDisplay: string[] = ['name', 'team', 'points', 'rebounds', 'blocks', 'steals', 'assists', 'fieldGoals', 'freeThrows', 'efficiency'];
   playerList: Player[] = [];
+  dataSource!: MatTableDataSource<Player>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private _playerService: PlayerService) {
-   }
+  }
 
   ngOnInit(): void {
-    this._playerService.getAllPlayers().subscribe(unpackedPlayers => this.playerList = unpackedPlayers);
+    this._playerService.getAllPlayers().subscribe(unpackedPlayers => this.playerList = unpackedPlayers,
+      error => console.log("Error" + error),
+      () => {
+        //executed once completed
+        this.dataSource = new MatTableDataSource<Player>(this.playerList);
+        this.dataSource.paginator = this.paginator;
+      });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator)
+    {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
