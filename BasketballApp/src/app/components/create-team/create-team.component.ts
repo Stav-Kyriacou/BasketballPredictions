d0ecfce73dcc4ac2f78b1eb7607bb4a61692f944
Stavros-Kyriacou <60002156+Stavros-Kyriacou@users.noreply.github.com>
2022-05-02 15:39:54 +1000

    refresh teams list after deleting

--- BasketballApp/src/app/components/create-team/create-team.component.ts
+++ BasketballApp/src/app/components/create-team/create-team.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Team } from 'src/app/models/team/team';
-import {PlayerService} from '../../services/player.service'
+import { PlayerService } from '../../services/player.service'
 
 @Component({
   selector: 'app-create-team',
-  templateUrl: './create-team.component.html', 
+  templateUrl: './create-team.component.html',
   styleUrls: ['./create-team.component.css']
 })
 export class CreateTeamComponent implements OnInit {
@@ -18,7 +18,7 @@ export class CreateTeamComponent implements OnInit {
   ngOnInit() {
     // Get all Teams data
     this._teamService.getAllTeams().subscribe(unpackedTeams => this.teams = unpackedTeams);
-    
+
 
   }
 
@@ -26,7 +26,7 @@ export class CreateTeamComponent implements OnInit {
     // send POST request with value(name of team) to API
     if (this.value != '') {
       this._teamService.postATeam(this.value).subscribe(value => value,
-        ()=>{
+        () => {
           // Reload list of teams
           this._teamService.getAllTeams().subscribe(unpackedTeams => this.teams = unpackedTeams);
         });
@@ -34,13 +34,14 @@ export class CreateTeamComponent implements OnInit {
   }
 
   // navigate to edit-team page with the team ID as the last /
-  editTeam(team:number) {
-    this.router.navigate(["edit-team",team]);   
+  editTeam(team: number) {
+    this.router.navigate(["edit-team", team]);
   }
   //deletes a team by their ID from API
-deleteTeam(TeamID:number) {
-  this._teamService.deleteTeam(TeamID).subscribe(data => {
-    console.log(data);
-    });
+  deleteTeam(TeamID: number) {
+    this._teamService.deleteTeam(TeamID).subscribe(data => data,
+      () => {
+        this._teamService.getAllTeams().subscribe(unpackedTeams => this.teams = unpackedTeams);
+      });
   }
 }
