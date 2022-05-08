feee0a7f25b2e09d404cd67a077d565abf2675d5
Adam-Dodson00 <62917191+Adam-Dodson00@users.noreply.github.com>
2022-05-08 12:43:37 +1000

    reformated app.module

--- BasketballApp/src/app/app.module.ts
+++ BasketballApp/src/app/app.module.ts
@@ -6,27 +6,12 @@ import { AppComponent } from './app.component';
 import { PlayerComponent } from './components/player/player.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSortModule } from '@angular/material/sort';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { CreateTeamComponent } from './components/create-team/create-team.component';
 import { FormsModule } from '@angular/forms';
-import { MatIconModule } from '@angular/material/icon';
 import { BannerComponent } from './components/banner/banner.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { EditTeamComponent } from './components/edit-team/edit-team.component'
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatDialogModule } from '@angular/material/dialog';
 import { SelectPlayer } from './components/edit-team/edit-team.component';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { PlayerTableComponent } from './components/player-table/player-table.component';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { AngularResizeEventModule } from 'angular-resize-event';
 import { MaterialModule } from './material.module';
 
@@ -46,22 +31,7 @@ import { MaterialModule } from './material.module';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSortModule,
-    MatToolbarModule,
-    MatProgressBarModule,
     FormsModule,
-    MatIconModule,
-    MatButtonModule,
-    MatExpansionModule,
-    MatGridListModule,
-    MatDialogModule,
-    MatSnackBarModule,
-    MatProgressSpinnerModule,
-    MatCheckboxModule,
     AngularResizeEventModule,
     MaterialModule
   ],
