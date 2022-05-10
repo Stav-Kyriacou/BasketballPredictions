31c915c517c02154304d3486b12cbae2c4feb1d2
Stavros-Kyriacou <60002156+Stavros-Kyriacou@users.noreply.github.com>
2022-05-10 15:37:12 +1000

    save team feedback and confirmation when leaving page

--- BasketballApp/src/app/components/edit-team/edit-team.component.ts
+++ BasketballApp/src/app/components/edit-team/edit-team.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, Inject, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Player } from 'src/app/models/player/player';
 import { Team } from 'src/app/models/team/team';
@@ -7,6 +7,8 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dial
 import { PlayerTableComponent } from '../player-table/player-table.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { TeamService } from 'src/app/services/team/team.service';
+import { ComponentCanDeactivate } from 'src/app/interfaces/component-can-deactivate';
+import { Observable } from 'rxjs';
 
 export interface AddPlayer {
   playerList: Player[];
@@ -20,13 +22,27 @@ let team: Team;
   templateUrl: './edit-team.component.html',
   styleUrls: ['./edit-team.component.css']
 })
-export class EditTeamComponent implements OnInit {
+export class EditTeamComponent implements OnInit, ComponentCanDeactivate {
   teamID: number;
   playerList: Player[] = [];
+  saving: boolean = false;
+  saved: boolean = true;
 
   @ViewChild(PlayerTableComponent) playerTable: PlayerTableComponent;
 
-  constructor(private router: Router, private route: ActivatedRoute, private _playerService: PlayerService, public dialog: MatDialog, private _teamService: TeamService) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private _teamService: TeamService,
+    private _playerService: PlayerService,
+    public dialog: MatDialog,
+    private _snackBar: MatSnackBar
+  ) { }
+  
+  @HostListener('window:beforeunload')
+  canDeactivate(): Observable<boolean> | boolean {
+    return this.saved;
+  }
 
   ngOnInit() {
     // get team ID from URL
@@ -46,19 +62,23 @@ export class EditTeamComponent implements OnInit {
 
   removePlayers() {
     let selectedPlayers = this.playerTable.selection.selected;
+    if (selectedPlayers.length <= 0) return;
 
     //Remove all selected players from the current team
     team.players = team.players.filter(function (player) {
       return !selectedPlayers.includes(player);
     });
-
+    this.saved = false;
     this.playerTable.setupTable(team.players);
   }
 
   saveTeam() {
+    this.saving = true;
     this._teamService.saveATeam(team).subscribe(value => value,
       () => {
-        this.router.navigate(["create-team"]);
+        // this.router.navigate(["create-team"]);
+        this.saving = false;
+        this.saved = true;
       });
   }
 
@@ -70,6 +90,9 @@ export class EditTeamComponent implements OnInit {
     })
     dialogRef.afterClosed().subscribe(result => {
       this.playerTable.setupTable(team.players);
+      if (result.event > 0) {
+        this.saved = false;
+      }
     });
   }
 }
@@ -97,20 +120,37 @@ export class SelectPlayer {
   }
 
   addNewPlayers() {
+    let selectedPlayers = this.playerTable.selection.selected;
+    if (selectedPlayers.length === 0) return;
+
+    let playersAdded = 0;
+    let duplicatePlayers = 0;
+    let message = "";
+
     if (team.players == null) {
       team.players = [];
     }
 
-    this.playerTable.selection.selected.forEach(player => {
-      team.players.push(player);
+    selectedPlayers.forEach(player => {
+      if (team.players.some(p => p.playerID === player.playerID)) {
+        duplicatePlayers++;
+      } else {
+        team.players.push(player);
+        playersAdded++;
+      }
     });
-
-    if (this.playerTable.selection.selected.length > 0) {
-      this._snackBar.open("Players have been added to the team", 'Okay', {
-        duration: 3000
-      });
+    if (playersAdded > 0 && duplicatePlayers > 0) {
+      message = "Players have been added, list contained some duplicate players";
+    } else if (playersAdded === 0 && duplicatePlayers > 0) {
+      message = "Can't add duplicate players";
+    } else if (playersAdded > 0 && duplicatePlayers === 0) {
+      message = "Players have been added to team";
     }
 
-    this.dialogRef.close();
+    this._snackBar.open(message, 'Okay', {
+      duration: 3000
+    });
+
+    this.dialogRef.close({ event: playersAdded });
   }
 }
