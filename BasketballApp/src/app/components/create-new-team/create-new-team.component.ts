8074af21e35635e9f2a86f29cb81679f49cffaec
JadMashtoub3 <80442672+JadMashtoub3@users.noreply.github.com>
2022-05-18 14:32:13 +1000

    changes

--- BasketballApp/src/app/components/create-new-team/create-new-team.component.ts
+++ BasketballApp/src/app/components/create-new-team/create-new-team.component.ts
@@ -4,6 +4,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { Team } from 'src/app/models/team/team';
 import { TeamService } from 'src/app/services/team/team.service';
+
 @Component({
   selector: 'app-create-new-team',
   templateUrl: './create-new-team.component.html',
@@ -17,6 +18,15 @@ export class CreateNewTeamComponent implements OnInit {
   teams: Team[] = [];
 
   constructor(private _formBuilder: FormBuilder, private _teamService: TeamService, private router: Router, public dialog: MatDialog) {}
+  onSubmit() {
+    // send POST request with value(name of team) to API
+    if (this.value != '') {
+      this._teamService.postATeam(this.value).subscribe(value => value,
+        ()=>{
+          this._teamService.getAllTeams().subscribe(unpackedTeams => this.teams = unpackedTeams);
+        });
+    }
+  }
 
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
@@ -27,15 +37,9 @@ export class CreateNewTeamComponent implements OnInit {
     });
     this._teamService.getAllTeams().subscribe(unpackedTeams => this.teams = unpackedTeams);
   }
-
-  onSubmit() {
-    // send POST request with value(name of team) to API
-    if (this.value != '') {
-      this._teamService.postATeam(this.value).subscribe(value => value,
-        ()=>{
-          // Reload list of teams
-          this._teamService.getAllTeams().subscribe(unpackedTeams => this.teams = unpackedTeams);
-        });
-    }
+  // navigate to edit-team page with the team ID as the last /
+  editTeam(team: number) {
+    this.router.navigate(["edit-team", team]);
   }
 }
+
