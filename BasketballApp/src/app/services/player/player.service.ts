2b7c86a5d3ca58a2cbe9022c12d0a651b261918a
Stav-Kyriacou <101610510@student.swin.edu.au>
2022-06-20 13:09:18 +1000

    use environment api url

--- BasketballApp/src/app/services/player/player.service.ts
+++ BasketballApp/src/app/services/player/player.service.ts
@@ -2,19 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Player } from '../../models/player/player';
-import { BaseService } from '../base/base.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 
-export class PlayerService extends BaseService {
+export class PlayerService {
 
   constructor(private _http: HttpClient) {
-    super();
   }
 
   getAllPlayers(): Observable<Player[]> {
-    return this._http.get<Player[]>(this.baseUrl + "/players");
+    return this._http.get<Player[]>(environment.api_url + "players");
   }
 }
--- BasketballApp/src/app/services/team/team.service.ts
+++ BasketballApp/src/app/services/team/team.service.ts
@@ -2,19 +2,18 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { Team } from 'src/app/models/team/team';
-import { BaseService } from '../base/base.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TeamService extends BaseService {
+export class TeamService {
 
   constructor(private _http: HttpClient) {
-    super();
   }
 
   getAllTeams(): Observable<Team[]> {
-    return this._http.get<Team[]>(this.baseUrl + "/teams")
+    return this._http.get<Team[]>(environment.api_url + "teams")
   }
 
   postATeam(teamName: string, userId: string): Observable<number> {
@@ -22,10 +21,10 @@ export class TeamService extends BaseService {
     const params = new HttpParams()
       .append('newTeam', teamName)
       .append('userID', userId);
-    return this._http.post<number>(this.baseUrl + "/create-team", body, { 'params': params })
+    return this._http.post<number>(environment.api_url + "create-team", body, { 'params': params })
   }
   getATeam(teamID: number): Observable<Team> {
-    return this._http.get<Team>(this.baseUrl + '/team/' + teamID)
+    return this._http.get<Team>(environment.api_url + 'team/' + teamID)
   }
 
   addPlayerToTeam(teamID: number, playerID: number, year: number): Observable<string> {
@@ -34,18 +33,18 @@ export class TeamService extends BaseService {
       .append('PlayerID', playerID)
       .append('Year', year)
     const body = ''
-    return this._http.post<string>(this.baseUrl + '/teamAllocation', body, { 'params': params })
+    return this._http.post<string>(environment.api_url + 'teamAllocation', body, { 'params': params })
   }
   deleteTeam(teamID: number): Observable<string> {
     const params = new HttpParams()
       .append('teamId', teamID)
-    return this._http.delete<string>(this.baseUrl + "/team/", { 'params': params })
+    return this._http.delete<string>(environment.api_url + "team/", { 'params': params })
   }
 
   saveATeam(team: Team): Observable<string> {
     const headers = { 'content-type': 'application/json' };
     const body = JSON.stringify(team);
-    return this._http.put<string>(this.baseUrl + '/teamAllocation', body, { 'headers': headers })
+    return this._http.put<string>(environment.api_url + 'teamAllocation', body, { 'headers': headers })
   }
   compareTeams(teamAId: number, teamBId: number): Observable<number> {
     // const headers = { 'content-type': 'application/json' };
@@ -53,6 +52,6 @@ export class TeamService extends BaseService {
     const params = new HttpParams()
       .append('teamAId', teamAId)
       .append('teamBId', teamBId);
-    return this._http.get<number>(this.baseUrl + '/compare', { 'params': params });
+    return this._http.get<number>(environment.api_url + 'compare', { 'params': params });
   }
 }
